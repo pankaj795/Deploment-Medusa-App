7b69b3fecdb36a0c9fc4c153d70750b9db156389
Pankaj Agarwal <64468444+pankaj795@users.noreply.github.com>
2024-04-02 12:00:56 +0530

    Update main.tf
    Updated code to use the farget as the backend.

--- main.tf
+++ main.tf
@@ -7,7 +7,13 @@ resource "aws_ecs_cluster" "medusa-cluster" {
 }
 
 resource "aws_ecs_task_definition" "medusa-task" {
-  family = "medusa-task"
+  family                   = "medusa-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  requires_compatibilities = ["FARGATE"]
   container_definitions = jsonencode([
     {
       name      = "medusa-container"
@@ -33,7 +39,75 @@ resource "aws_ecs_service" "medusa-service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [var.subnet_id]
-    security_groups = [var.security_group_id]
+    assign_public_ip = true
+    subnets          = var.subnets
+    security_groups  = var.security_groups
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.medusa-tg.arn
+    container_name   = "medusa-container"
+    container_port   = 3000
+  }
+}
+
+resource "aws_lb" "medusa-lb" {
+  name               = "medusa-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.lb_security_groups
+  subnets            = var.subnets
+}
+
+resource "aws_lb_target_group" "medusa-tg" {
+  name     = "medusa-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "medusa-listener" {
+  load_balancer_arn = aws_lb.medusa-lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.medusa-tg.arn
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "ecs_task_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
 }
